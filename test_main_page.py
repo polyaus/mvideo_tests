67cfe4e31523513e77bc8dc42f500c937e609f04
Polya U <ex_tovar@mail.ru>
2021-08-13 15:47:50 +0500

    Add test_favorite_list_is_empty to MainPage

--- test_main_page.py
+++ test_main_page.py
@@ -6,3 +6,11 @@ def test_change_location_on_main_page(browser, main_page_url):
     page.open()
     page.change_location_to_kaliningrad()
     page.check_location_is_kaliningrad()
+
+
+def test_favorite_list_is_empty(browser, main_page_url):
+    page = MainPage(browser, main_page_url)
+    page.open()
+    page.open_favorite_list_from_main_page()
+    page.favorite_list_is_empty()
+
