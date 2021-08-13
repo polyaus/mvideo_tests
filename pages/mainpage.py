dbbbdb4cf3269f848ea2e134d431bb85f8fad40d
Polya U <ex_tovar@mail.ru>
2021-08-13 19:14:43 +0500

    Refactoring tests - semantic value is enters - step 2

--- pages/mainpage.py
+++ pages/mainpage.py
@@ -34,7 +34,6 @@ class MainPage:
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
-
         except NoSuchElementException:
             return False
 
@@ -43,7 +42,6 @@ class MainPage:
     def is_not_element_present(self, how, what, timeout=4):
         try:
             WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
-
         except TimeoutException:
             return True
 
