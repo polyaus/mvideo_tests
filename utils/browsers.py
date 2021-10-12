252f4d9abc82474da30554c0555bac56c3339a01
Polya U <64306872+polyaus@users.noreply.github.com>
2021-10-12 21:24:44 +0500

    Auto install ChromeDriver

--- utils/browsers.py
+++ utils/browsers.py
@@ -10,4 +10,4 @@ def build_browser():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
-    return webdriver.Chrome(chrome_options=chrome_options)
+    return webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
