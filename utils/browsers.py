84c039610ca4b6b1a4ad4b107f298a2dec2372b3
Polya U <64306872+polyaus@users.noreply.github.com>
2021-10-12 20:39:03 +0500

    Run Selenium without headless

--- utils/browsers.py
+++ utils/browsers.py
@@ -7,8 +7,7 @@ def build_browser():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_experimental_option("prefs", prefs)
     chrome_options.add_argument('--window-size=1320,1080')
-    chrome_options.add_argument("--no-sandbox");
-    chrome_options.add_argument("--disable-dev-shm-usage");
-    chrome_options.add_argument("--headless");
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
 
     return webdriver.Chrome(chrome_options=chrome_options)
