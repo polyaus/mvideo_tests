784c1108c299cd5fb9e0a4a1b74862dc2a98654a
Polya U <ex_tovar@mail.ru>
2021-10-18 18:50:17 +0500

    Debugging login test - step 2

--- test_login_page.py
+++ test_login_page.py
@@ -26,5 +26,4 @@ class TestsFromLoginPage:
         login_page = LoginPage(self.browser, login_page_url)
         login_page.open_authorization_form()
         login_page.enter_user_data_in_authorization_form()
-        login_page.open_user_cabinet()
         login_page.check_user_is_logged()
