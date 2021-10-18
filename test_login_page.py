1aa411bfb3c79a49543489430b4e3346ac5d3f22
Polya U <ex_tovar@mail.ru>
2021-10-18 18:07:24 +0500

    Fix run tests on CI.

--- test_login_page.py
+++ test_login_page.py
@@ -1,3 +1,7 @@
+import os
+
+from pyvirtualdisplay import Display
+
 from pages.login_page import LoginPage
 from utils.browsers import build_browser
 
@@ -5,11 +9,18 @@ from utils.browsers import build_browser
 class TestsFromLoginPage:
     @classmethod
     def setup_class(cls):
+        if os.getenv("in_ci", False):
+            cls.display = Display(visible=0, size=(1920, 1080))
+            cls.display.start()
+
         cls.browser = build_browser()
 
     @classmethod
     def teardown_class(cls):
         cls.browser.quit()
+
+        if os.getenv("in_ci", False):
+            cls.display.stop()
 
     def test_user_logged(self, login_page_url):
         login_page = LoginPage(self.browser, login_page_url)
--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,7 @@
+import os
+
+from pyvirtualdisplay import Display
+
 from pages.mainpage import MainPage
 from utils.browsers import build_browser
 from utils.pages import main_page_close_ad
@@ -6,12 +10,19 @@ from utils.pages import main_page_close_ad
 class TestsFromMainPage:
     @classmethod
     def setup_class(cls):
+        if os.getenv("in_ci", False):
+            cls.display = Display(visible=0, size=(1920, 1080))
+            cls.display.start()
+
         cls.browser = build_browser()
         main_page_close_ad(cls.browser)
 
     @classmethod
     def teardown_class(cls):
         cls.browser.quit()
+
+        if os.getenv("in_ci", False):
+            cls.display.stop()
 
     def test_change_location_on_main_page(self, main_page_url):
         main_page = MainPage(self.browser, main_page_url)
